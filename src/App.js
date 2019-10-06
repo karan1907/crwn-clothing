24a9195b8c218715a42b506fb5b181450e46ff3b
Goldy King <goldyking@Goldys-MacBook-Pro.local>
2019-10-06 10:20:45 +0530

    broken homepage into smaller components, finished styling our menu items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./Components/homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
